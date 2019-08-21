bc82d738111ffe8717f4c9af970e3f3188c9627e
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-21 10:41:22 -0400

    day's stats

--- screens/StatsAll.js
+++ screens/StatsAll.js
@@ -12,7 +12,7 @@ import PieChart from "./components/PieChart";
 import styles from "./style/style";
 import axios from "axios";
 import SCREENS from "../constants";
-import psl from "psl";
+import XDate from "../xdate";
 
 export default function StatsAll(props) {
   const { navigation } = props;
@@ -26,6 +26,45 @@ export default function StatsAll(props) {
     getStats();
   }, [labels, series]);
 
+  function getDateString(date) {
+    if (Platform.OS === "ios")
+      return date.toLocaleDateString("en-US", {
+        weekday: "short",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+      });
+    else {
+      var dayOfWeek = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
+        monthName = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ],
+        utc = date.getTime() + date.getTimezoneOffset() * 60000,
+        US_time = utc + 3600000 * -4,
+        US_date = new Date(US_time);
+
+      return (
+        dayOfWeek[US_date.getDay() - 1] +
+        ", " +
+        monthName[US_date.getMonth()] +
+        " " +
+        US_date.getDate() +
+        ", " +
+        US_date.getFullYear()
+      );
+    }
+  }
   async function getStats() {
     // setId(userId);
     // console.log("in get stats");
@@ -37,12 +76,15 @@ export default function StatsAll(props) {
         // console.log("DATA!", data);
         if (data.success) {
           const stats = data.stats;
-          // console.log(stats, "STAT");
-          stats.forEach(item => console.log(item.time));
-          let allLabels = stats.map(item => item.url.slice(8)).slice(0, 10);
-          // console.log(allLabels);
+          // let dayArr = stats.filter(
+          //   item => item.date == new Date(new Date().toLocaleDateString())
+          // );
+          // console.log(new Date(new Date().toLocaleDateString()));
+          console.log("DATEX", new XDate());
+          console.log(getDateString(new Date()));
+          // stats.forEach(item => console.log(item.time));
 
-          await setLabels(allLabels);
+          await setLabels(stats.map(item => item.url).slice(0, 10));
 
           await setSeries(stats.map(item => Math.ceil(item.time / 60)).slice(0, 10));
         }
@@ -57,7 +99,7 @@ export default function StatsAll(props) {
 
   return (
     <View style={styles.containerStats}>
-      <Text style={styles.containerText}>Your stats for the today: </Text>
+      <Text style={styles.containerText}>Your Cumulative Stats: </Text>
       <PieChart userLabels={labels} userSeries={series} />
     </View>
   );
